bce18e58a93996bf590da6ed0f832b7c4fa139a3
David <davidventura27@gmail.com>
2019-01-01 13:17:18 +0100

    map values from WS to the ones required by the protocol

--- web/ws.py
+++ web/ws.py
@@ -3,8 +3,8 @@ import json
 import time
 import redis
 
-from divoom.image import solid_color
-from divoom.protocol import Commands
+from divoom.image import solid_color, _pack_tuples_to_image
+from divoom.protocol import Commands, Command, freq_to_bytes
 from queue import Queue
 from threading import Thread
 from websocket_server import WebsocketServer
@@ -19,6 +19,13 @@ def str_to_command(command):
         if c.name == command:
             return c
 
+def transform_value(command, value):
+    if command == Commands.STATIC_IMG:
+        return _pack_tuples_to_image(value)
+    elif command == Commands.SET_RADIO:
+        return freq_to_bytes(value)
+    else:
+        return value
 def msg_received_ws(q):
     def wrap(client, server, msg):
         try:
@@ -35,8 +42,10 @@ def msg_received_ws(q):
             print("Invalid command: %s" % msg)
             return
 
+        value = transform_value(command, msg['value'])
         print("Got %s from a ws client; putting to queue" % msg)
-        q.put((command, msg['value']))
+        print(value)
+        q.put((command, value))
     return wrap
 
 def handle_replies_redis(r_q):
@@ -58,15 +67,20 @@ def main():
     t.daemon = True
     t.start()
 
-    r = redis.Redis(host='localhost', port=6379, db=0)
+    r = redis.Redis(host='octopi.labs', port=6379, db=0)
     p = r.pubsub()
     p.subscribe(replies=handle_replies_redis(r_q))
 
     while True:
         if not ws_q.empty():
-            data = ws_q.get()
-            print('from ws', data)
-            #r.publish('commands', bytes(data))
+            comm, data = ws_q.get()
+            print('from ws', comm, data)
+            if type(data) is list:
+                c = Command(comm, None, data)
+            else:
+                c = Command(comm, None, [data])
+            print(c, c.command)
+            r.publish('commands', bytes(c.command))
         if not r_q.empty():
             data = r_q.get()
             print('from redis', data)
